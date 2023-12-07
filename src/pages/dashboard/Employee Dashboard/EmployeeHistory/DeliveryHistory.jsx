77069f9dd1d925074e8b0eb0abbdc714a85d8db7
Abu Taher <writerabutaher@gmail.com>
2023-12-07 11:17:08 +0600

    next project created

--- src/pages/dashboard/Employee Dashboard/EmployeeHistory/DeliveryHistory.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const DeliveryHistory = () => {
-    return (
-        <div>DeliveryHistory</div>
-    )
-}
-
-export default DeliveryHistory
--- src/pages/dashboard/Employee Dashboard/EmployeeHistory/EarningHistory.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const EarningHistory = () => {
-    return (
-        <div>EarningHistory</div>
-    )
-}
-
-export default EarningHistory
