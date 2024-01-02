0e468dacd6b8fc279661f9dea40f62643a5ab937
Abu Taher <writerabutaher@gmail.com>
2024-01-02 18:02:56 +0600

    invoice page

---
+++ src/app/dashboard/admin/invoices/page.tsx
@@ -0,0 +1,23 @@
+import InvoiceTable from "@/components/Dashboard/Invoices/InvoiceTable";
+import React from "react";
+
+const InvoicesPage = () => {
+  return (
+    <div className="max-w-screen-xl mx-auto px-6 py-10">
+      <div className="pb-10 space-y-2">
+        <h1 className="text-2xl font-semibold text-gray-600 capitalize lg:text-3xl">
+          All <span className="text-primary">INVOICES</span>
+        </h1>
+
+        <>
+          <span className="inline-block w-20 h-1 bg-primary rounded-full"></span>
+          <span className="inline-block w-4 h-1 ml-1 bg-primary rounded-full"></span>
+          <span className="inline-block w-2 h-1 ml-1 bg-primary rounded-full"></span>
+        </>
+      </div>
+      <InvoiceTable />
+    </div>
+  );
+};
+
+export default InvoicesPage;
